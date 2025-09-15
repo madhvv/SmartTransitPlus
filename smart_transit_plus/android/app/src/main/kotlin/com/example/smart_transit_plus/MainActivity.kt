ce6ce53573bfce9284b9143fcc6da3b84da48890
darkkieee <balumohan04@gmail.com>
2025-09-16 02:05:55 +0530

    Added bus movements,source/destination,ETA and number of ppl in the bus

---
+++ smart_transit_plus/android/app/src/main/kotlin/com/example/smart_transit_plus/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.smart_transit_plus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ smart_transit_plus/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'map_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Smart Transit Plus',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const GetStartedPage(),
+    );
+  }
+}
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade50,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.directions_bus, size: 100, color: Colors.blue),
+              const SizedBox(height: 30),
+              const Text(
+                "Welcome to Smart Transit Plus",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Track buses in real-time with crowd info.\nTap below to get started!",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, color: Colors.black54),
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 40,
+                    vertical: 15,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MapScreen()),
+                  );
+                },
+                child: const Text(
+                  "Get Started",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
